51fb6cc2ff45a15c19e6ed8b2f38147efa34bfe5
Mohamed-elg <mohamed.elguermat9@gmail.com>
2023-11-27 16:51:42 +0100

    set up basic api endpoint

---
+++ api/src/main/java/com/bbai/api/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.bbai.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class MainController {
+
+    @GetMapping("/")
+    public ApiResponse mainRoute() {
+        ApiResponse response = new ApiResponse();
+        response.setCode(200);
+        response.setData("Hello World ! API is working");
+        return response;
+    }
+
+    @JsonPropertyOrder({ "code", "data" })
+    static class ApiResponse {
+        private int Code;
+        private String data;
+
+        public int getCode() {
+            return Code;
+        }
+
+        public void setCode(int code) {
+            this.Code = code;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
+}
