211bbb1eb2762e7083a902e959bb4b4fc03b2bec
Adèle Mourier <100832619+DD23154@users.noreply.github.com>
2023-11-29 10:08:41 +0100

    creation navbar and first routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MechanicalSheetComponent } from './mechanical-sheet/mechanical-sheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mechanical-sheet', component: MechanicalSheetComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
