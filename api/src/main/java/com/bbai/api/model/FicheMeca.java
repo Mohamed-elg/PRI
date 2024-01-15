202cf91669afe4ba4cfc6a2eaed10b8573d6c188
Cyril <cyril.bier42100@gmail.com>
2024-01-15 16:29:26 +0100

    FicheMecanique controller

---
+++ api/src/main/java/com/bbai/api/model/FicheMeca.java
@@ -0,0 +1,29 @@
+package com.bbai.api.model;
+
+import com.bbai.api.model.Client.ClientModel;
+import com.bbai.api.model.assemblage.Assemblage;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class FicheMeca {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private LocalDate dateCreation;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Assemblage assemblage;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private ClientModel client;
+
+    private LocalDate lastModification;
+
+    private String modifiedBy;
+
+}
