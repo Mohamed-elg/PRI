b1f7499d93a0db4e2724f83094a43a3981a4c2f6
Adèle Mourier <100832619+DD23154@users.noreply.github.com>
2024-01-15 09:55:31 +0100

    get inputs from form and display in console

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +36,9 @@ import { ListMechanicalSheetsComponent } from './list-mechanical-sheets/list-mec
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/connection/connection.component.ts
+++ web/src/app/connection/connection.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'connection',
   templateUrl: './connection.component.html',
   styleUrls: ['./connection.component.css']
 })
-export class ConnectionComponent {
+export class ConnectionComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit(): void {
+      this.loginForm = this.fb.group({
+        inputId:   ['', Validators.required],
+        inputPassword: ['', Validators.required]
+      });
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      const formData = this.loginForm.value;
+
+      console.log(formData);
+
+      /*
+      this.http.post('[api_endpoint]', formData).subscribe(
+        (response) => {
+          console.log('Login successful',response);
+        },
+        (error) => {
+          console.error('Login failed', error);
+        }
+      );
+      */
+    }
+  }
 
 }
