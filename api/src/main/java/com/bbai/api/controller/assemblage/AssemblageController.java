c3b506c12d7fa88a366fe6928bf0f9ca4cee4303
Cyril <cyril.bier42100@gmail.com>
2024-01-15 14:50:49 +0100

    Authorization check in AssemblageController

--- api/src/main/java/com/bbai/api/controller/assemblage/AssemblageController.java
+++ api/src/main/java/com/bbai/api/controller/assemblage/AssemblageController.java
@@ -2,14 +2,15 @@ package com.bbai.api.controller.assemblage;
 
 import com.bbai.api.model.assemblage.Assemblage;
 import com.bbai.api.repository.assemblage.AssemblageRepository;
+import com.bbai.api.service.Account.accountService;
+import com.bbai.api.service.Account.tokenValidatorService;
 import com.bbai.api.service.assemblage.AssemblageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api")
@@ -17,12 +18,20 @@ public class AssemblageController {
     @Autowired
     AssemblageService assemblageService;
 
+    @Autowired
+    private tokenValidatorService tokenService;
+
     @PostMapping("/assemblage")
-    public ResponseEntity<Assemblage> postAssemblage(@RequestBody Assemblage assemblage){
+    public ResponseEntity<Object> postAssemblage(@RequestBody Assemblage assemblage, @RequestHeader("Authorization") String authorizationHeader){
         try {
-            return new ResponseEntity<>(assemblageService.saveAssemblage(assemblage), HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(assemblageService.saveAssemblage(assemblage,tokenService.getTokenFromHeader(authorizationHeader)), HttpStatus.ACCEPTED);
         }catch(Exception e){
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            if(e instanceof AccessDeniedException){
+                return new ResponseEntity<>(e.toString(), HttpStatus.UNAUTHORIZED);
+            }else{
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
         }
     }
 }
--- api/src/main/java/com/bbai/api/service/assemblage/AssemblageService.java
+++ api/src/main/java/com/bbai/api/service/assemblage/AssemblageService.java
@@ -1,17 +1,32 @@
 package com.bbai.api.service.assemblage;
 
+import com.bbai.api.model.Account.accountModel;
 import com.bbai.api.model.assemblage.Assemblage;
 import com.bbai.api.repository.assemblage.AssemblageRepository;
+import com.bbai.api.service.Account.accountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.nio.file.AccessDeniedException;
 
 @Service
 public class AssemblageService {
     @Autowired
     AssemblageRepository assemblageRepository;
 
-    public Assemblage saveAssemblage(Assemblage assemblage){
+    @Autowired
+    accountService accountService;
+
+
+    public Assemblage saveAssemblage(Assemblage assemblage, String token) throws AccessDeniedException {
+
+        if (accountService.getAccountByToken(token).isEmpty()) {
+            throw new AccessDeniedException("Access is forbidden: invalid token");
+        }
+
         return assemblageRepository.save(assemblage);
     }
+
 
 }
