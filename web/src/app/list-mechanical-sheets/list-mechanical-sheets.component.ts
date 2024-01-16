e605c0385edcac371edc70b7851bed6ca47fcf82
Adèle Mourier <100832619+DD23154@users.noreply.github.com>
2024-01-16 21:40:08 +0100

    get and display list of mechanical sheets

--- web/src/app/list-mechanical-sheets/list-mechanical-sheets.component.ts
+++ web/src/app/list-mechanical-sheets/list-mechanical-sheets.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'list-mechanical-sheets',
   templateUrl: './list-mechanical-sheets.component.html',
   styleUrls: ['./list-mechanical-sheets.component.css']
 })
-export class ListMechanicalSheetsComponent {
+export class ListMechanicalSheetsComponent implements OnInit{
 
+  mechanical_sheet_url = 'http://localhost:8081/api/FicheMecanique';
+
+  list_sheets: any;  
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const authToken = localStorage.getItem('authToken');
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${authToken}`
+    });
+
+      this.http.get(this.mechanical_sheet_url, { headers }).subscribe( response => {
+        this.list_sheets = response;
+      }, error => {
+        console.error('Error in get request:', error);
+      })
+  }
 }
