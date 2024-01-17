4797d4b52497c8009b11807cd1228c874e744012
Cyril <cyril.bier42100@gmail.com>
2024-01-17 10:16:09 +0100

    Swagger doc for FicheMecaController

--- api/src/main/java/com/bbai/api/controller/FicheMecaController.java
+++ api/src/main/java/com/bbai/api/controller/FicheMecaController.java
@@ -3,6 +3,11 @@ package com.bbai.api.controller;
 import com.bbai.api.dto.FicheMecaDTO;
 import com.bbai.api.service.FicheMecaService;
 import com.bbai.api.service.account.TokenValidatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,7 @@ import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Fiche Mecanique API")
 public class FicheMecaController {
     @Autowired
     private FicheMecaService ficheMecaService;
@@ -19,9 +25,21 @@ public class FicheMecaController {
     @Autowired
     private TokenValidatorService tokenService;
 
+    @Operation(summary = "Create a Fiche Mecanique",
+            description = "Create a new Fiche Mecanique corresponding to the body ")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "202",description = "Succesfully created"),
+            @ApiResponse(responseCode = "401",description = "Bad permissions"),
+            @ApiResponse(responseCode = "500",description = "Internal error")
+    })
     @PostMapping("/FicheMecanique")
-    public ResponseEntity<Object> postFicheMeca(@RequestBody FicheMecaDTO ficheMecaDTO,
-            @RequestHeader("Authorization") String authorizationHeader) {
+    public ResponseEntity<Object> postFicheMeca(
+            @RequestBody
+            @Parameter(name = "Fiche Mecanique", description = "The Fiche Mecanique you want to create")
+            FicheMecaDTO ficheMecaDTO,
+            @RequestHeader("Authorization")
+            @Parameter(name = "Access Token", description = "Bearer token corresponding to an existing account")
+            String authorizationHeader) {
         try {
             return new ResponseEntity<>(
                     ficheMecaService.saveFicheMeca(ficheMecaDTO, tokenService.getTokenFromHeader(authorizationHeader)),
